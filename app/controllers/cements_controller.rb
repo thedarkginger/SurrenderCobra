475644f36a86fac40666a4b319f62262a85733a2
thedarkginger <jp.gooderham@gmail.com>
2016-08-01 15:10:32 -0700

    new pathway works locally trying heroku

--- app/controllers/cements_controller.rb
+++ app/controllers/cements_controller.rb
@@ -1,18 +1,14 @@
 class CementsController < ApplicationController
 
-	def new
-		@cements = Cement.new
-	end
-
 	def create
-  		@cements= Cement.new(ftwblog_params)
+  		@cements= Cement.new(story_params)
   		@cements.save
   		redirect_to(:back)
 	end
 
 	private
 
-  	def ftwblog_params
+  	def story_params
     	params.require(:cements).permit(:username, :post, :slug)
   	end
 
