daf4a2cb77e5d400d6a89a173905df6022923dfd
thedarkginger <jp.gooderham@gmail.com>
2016-08-01 13:11:13 -0700

    form issue

--- app/controllers/fearthewaves_controller.rb
+++ app/controllers/fearthewaves_controller.rb
@@ -2,14 +2,27 @@ class FearthewavesController < ActionController::Base
 	layout "ftwlayouts"
 
 	def index
-	 @posts = Post.order('updated_at DESC').all
-	 @comments = Cement.all
+		@posts = Post.order('updated_at DESC').all
+		@comments = Cement.all
 	end
 
 	def fearthewave
-		@posts = Post.where(slug: params[:id])
-		@comments = Cement.where(slug: params[:id]).all
+		@posts = Post.where(slug: params[:id]).all
+		@comments = Cement.all
+	end 
+
+  	def create
+  		@cements= Cement.new(story_params)
+  		@cements.save
+  		redirect_to(:back)
 	end
+
+	private
+
+  	def story_params
+    	params.require(:cements).permit(:username, :post, :slug)
+  	end
+
 
 end
 
