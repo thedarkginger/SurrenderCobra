bf5cde895785e9dc2913ffe620763f38df313bda
thedarkginger <jp.gooderham@gmail.com>
2016-08-06 17:24:22 -0700

    front page test 2

--- app/controllers/fearthewaves_controller.rb
+++ app/controllers/fearthewaves_controller.rb
@@ -2,7 +2,7 @@ class FearthewavesController < ActionController::Base
 	layout "ftwlayouts"
 
 	def index
-		@posts = Post.order(published_at: :desc).last(5)
+		@posts = Post.order('created_at DESC').last(5)
 		@comments = Cement.where(slug: params[:id]).all
 	end
 
